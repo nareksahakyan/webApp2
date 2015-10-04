b21604660e0b0aab3d9d84a2e0a7d9915a7e50ca
nareksahakyan <nareksahakyan@gmx.com>
2015-10-04 19:23:39 +0400

    web_app2

---
+++ WebApplication1/Controllers/adminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    
+    public class adminController : Controller
+    {
+        //
+        // GET: /admin/
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+
+            var users = db.Users.ToList();
+
+            ViewBag.usr = users;
+
+            return View();
+        }
+	}
+}
